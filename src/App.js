269ffeb0f13c0fff921f4539b00a5bddb9aef557
Saurabh <ss6408367@gmail.com>
2022-06-26 22:14:46 +0530

    updated my branch

--- src/App.js
+++ src/App.js
@@ -9,11 +9,7 @@ import SocialLinks from "./components/SocialLinks";
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="w-full dark:bg-gray-900 bg-gray-200 text-gray-900 dark:text-gray-300">
-=======
     <div className="w-full bg-gray-900 text-gray-300">
->>>>>>> main
       <Navbar />
       <Home />
       <About />
