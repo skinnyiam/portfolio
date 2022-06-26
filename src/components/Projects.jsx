47d63db25459b31f51ba5545ea9d734017d80458
Saurabh Shukla <84450605+skinnyiam@users.noreply.github.com>
2022-06-26 21:47:30 +0530

    added new project section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,17 +1,56 @@
 import React from "react";
 import ProjectImage from "../asset/movie.png";
+import RecipeasImage from "../asset/recipeas.png"
 
 const Projects = () => {
   return (
     <div id="projects" className="w-full">
       <div className="w-full flex justify-center my-8">
         <div className="w-full">
-          <h1 className="sm:text-2xl lg:text-4xl text-xl text-gray-50 font-semibold text-center p-0 sm:py-4">
+          <h1 className="sm:text-2xl lg:text-4xl text-xl dark:text-gray-50 text-gray-600 font-semibold text-center p-0 sm:py-4">
             PROJECTS
           </h1>
           <div
             className="flex flex-col sm:flex-row justify-between items-start w-full px-4 sm:px-16 py-2"
             data-aos="fade-right"
+          >
+            <img
+              src={RecipeasImage}
+              alt=""
+              className="w-full sm:w-1/3 lg:w-1/6 max-w-[20rem] h-[150px] rounded"
+            />
+            <div className="py-4 sm:py-0 sm:px-4 w-full sm:w-2/3 lg:w-5/6 ">
+              <h2 className="text-green-400 text-sm sm:text-base lg:text-lg font-bold">
+                REACT+NEXTJS (Tailwind) APP
+              </h2>
+              <h3 className="dark:text-gray-50 text-gray-600 text-lg sm:text-xl lg:text-2xl font-bold">
+                RECIPEAS WEB APP
+              </h3>
+              <p className="text-sm sm:text-base lg:text-lg">
+                A platform where users can search a wide range of recipeas with full instructions
+                and ingredients. Users can also save their favourite recipeas by logging in so 
+                they dont get forget their favourite meals..
+
+              </p>
+              <div className="flex justify-start items-center my-2">
+                <a
+                  className="font-bold text-gray-50 bg-green-400 rounded px-3 py-2 mr-2"
+                  href="https://recipeas-zeta.vercel.app/"
+                >
+                  WEBSITE
+                </a>
+                <a
+                  className="font-bold text-green-400 bg-transparent rounded px-2 py-1 outline outline-green-400 ml-2"
+                  href="https://github.com/skinnyiam/Recipeas"
+                >
+                  GITHUB
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            className="flex flex-col mt-6 sm:flex-row justify-between items-start w-full px-4 sm:px-16 py-2"
+            data-aos="fade-right "
           >
             <img
               src={ProjectImage}
@@ -22,7 +61,7 @@ const Projects = () => {
               <h2 className="text-green-400 text-sm sm:text-base lg:text-lg font-bold">
                 REACT APP
               </h2>
-              <h3 className="text-gray-50 text-lg sm:text-xl lg:text-2xl font-bold">
+              <h3 className="dark:text-gray-50 text-gray-600 text-lg sm:text-xl lg:text-2xl font-bold">
                 MOVIE RATING APP
               </h3>
               <p className="text-sm sm:text-base lg:text-lg">
@@ -45,6 +84,7 @@ const Projects = () => {
               </div>
             </div>
           </div>
+          
         </div>
       </div>
     </div>
