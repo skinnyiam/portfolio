b6ea24c937a10bdd545c17c6de1dda2a1e39f787
Saurabh <ss6408367@gmail.com>
2022-09-22 20:36:49 +0530

    updated resume link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
             <li className="w-40 h-14 flex justify-between items-center translate-x-[-7.5rem] hover:translate-x-0 duration-300 bg-green-400 text-white px-2">
               <a
                 className="flex justify-between items-center w-full font-bold"
-                href="/"
+                href="https://drive.google.com/file/d/1DvWK5JR_Eas0lcSOxBLu7FAuOHyK2QPg/view"
               >
                 Resume
               </a>
